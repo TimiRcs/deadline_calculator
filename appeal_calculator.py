ba3e4b934bd53e18379324aabc81644bac24c0cc
Tímea Ratatics <ratatimi@gmail.com>
2023-09-01 12:30:55 +0200

    second version

--- appeal_calculator.py
+++ appeal_calculator.py
@@ -1,109 +1,108 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 
-import os, sys
 from datetime import date, timedelta
 from dateutil.easter import easter
 
-startd = input("Irja be a hatarozat idopontjat kotojelekkel elvalasztva: ")
-startday = date.fromisoformat(startd)
-  
-def PlusDays():
-    global endday
-    endday = startday + timedelta(days=15)
-    return endday
-
-
-def WeekEnd():
-    global endday
-    what_d = date.isoweekday(endday)
-    if what_d == 6:
-        endday = endday + timedelta(days=2)
-            
-    elif what_d == 7:
-        endday = endday + timedelta(days=1)
-            
-    return endday    
-
-
-def Holidays():
-    PlusDays()
-    WeekEnd()
-    JudicalVacation()
+class AppealCalculator:
     
-    this_year = startday.year
-    easter_sunday = easter(this_year, 3)
-    easter_mon = easter_sunday + timedelta(days=1)
-    easter_sat = easter_sunday - timedelta(days=1)
-    easter_fri = easter_sunday - timedelta(days=2)
-    punkosd = easter_sunday + timedelta(days=50)
-
-    global endday
-    
-    # március 15.
-    if endday == date.fromisoformat(f"{this_year}-03-15"):
-        endday = endday + timedelta(days=1)
-                
-    # munka ünnepe
-    elif endday == date.fromisoformat(f"{this_year}-05-01"):
-        endday = endday + timedelta(days=1)
-                
-    # október 23.
-    elif endday == date.fromisoformat(f"{this_year}-10-23"):
-        endday = endday + timedelta(days=1)
-                
-    # mindenszentek
-    elif endday == date.fromisoformat(f"{this_year}-11-01"):
-        endday = endday + timedelta(days=1)
-                
-    #húsvét
-    elif endday == easter_sunday:
-        endday = easter_sunday + timedelta(days=2)
-                
-    elif endday == easter_mon:
-        endday = easter_sunday + timedelta(days=2)
-                
-    elif endday == easter_sat:
-        endday = easter_sunday + timedelta(days=2)
-                
-    elif endday == easter_fri:
-        endday = easter_sunday + timedelta(days=2)
-                   
-    #pünkösd
-    elif endday == punkosd:
-        endday = endday + timedelta(days=1)
+    def __init__(self, startd: str):
+        self.startd = startd
         
-    WeekEnd()
+        self.startday = date.fromisoformat(startd)
+    
+    def PlusDays(self):
+        self.endday = self.startday + timedelta(days=15)
+        return self.endday
+
+
+    def WeekEnd(self):
+        what_d = date.isoweekday(self.endday)
+        if what_d == 6:
+            self.endday = self.endday + timedelta(days=2)
+                
+        elif what_d == 7:
+            self.endday = self.endday + timedelta(days=1)
+                
+        return self.endday    
+
+
+    def Holidays(self):
+        self.PlusDays()
+        self.WeekEnd()
+        self.JudicalVacation()
         
-    return endday
+        this_year = self.startday.year
+        easter_sunday = easter(this_year, 3)
+        easter_mon = easter_sunday + timedelta(days=1)
+        easter_sat = easter_sunday - timedelta(days=1)
+        easter_fri = easter_sunday - timedelta(days=2)
+        punkosd = easter_sunday + timedelta(days=50)
+        
+        # március 15.
+        if self.endday == date.fromisoformat(f"{this_year}-03-15"):
+            self.endday = self.endday + timedelta(days=1)
+                    
+        # munka ünnepe
+        elif self.endday == date.fromisoformat(f"{this_year}-05-01"):
+            self.endday = self.endday + timedelta(days=1)
+                    
+        # október 23.
+        elif self.endday == date.fromisoformat(f"{this_year}-10-23"):
+            self.endday = self.endday + timedelta(days=1)
+                    
+        # mindenszentek
+        elif self.endday == date.fromisoformat(f"{this_year}-11-01"):
+            self.endday = self.endday + timedelta(days=1)
+                    
+        #húsvét
+        elif self.endday == easter_sunday:
+            self.endday = easter_sunday + timedelta(days=2)
+                    
+        elif self.endday == easter_mon:
+            self.endday = easter_sunday + timedelta(days=2)
+                    
+        elif self.endday == easter_sat:
+            self.endday = easter_sunday + timedelta(days=2)
+                    
+        elif self.endday == easter_fri:
+            self.endday = easter_sunday + timedelta(days=2)
+                    
+        #pünkösd
+        elif self.endday == punkosd:
+            self.endday = self.endday + timedelta(days=1)
+            
+        self.WeekEnd()
+            
+        return self.endday
 
 
-def JudicalVacation():
-    global endday
-    this_year = startday.year
-    next_year = (endday + timedelta(weeks=52)).year
-    
-    #nyári
-    if endday.month == 7:
-        days_left1 = endday.day - 14
-        if days_left1 > 0:
-            endday = date.fromisoformat(f"{this_year}-08-20") + timedelta(days=days_left1)
-    
-    #téli
-    elif endday.month == 12:
-        days_left2 = endday.day - 23
-        if days_left2 > 0:
-            endday = date.fromisoformat(f"{next_year}-01-01") + timedelta(days=days_left2)
-    
-    WeekEnd()
-    
-    return endday
+    def JudicalVacation(self):
+        this_year = self.startday.year
+        next_year = (self.endday + timedelta(weeks=52)).year
+        
+        #nyári
+        if self.endday.month == 7:
+            days_left1 = self.endday.day - 14
+            if days_left1 > 0:
+                self.endday = date.fromisoformat(f"{this_year}-08-20") + timedelta(days=days_left1)
+        
+        #téli
+        elif self.endday.month == 12:
+            days_left2 = self.endday.day - 23
+            if days_left2 > 0:
+                self.endday = date.fromisoformat(f"{next_year}-01-01") + timedelta(days=days_left2)
+        
+        self.WeekEnd()
+        
+        return self.endday
 
 
-def WriteDate():
-    global endday
-    Holidays()
-    print(f"A hatarido vege {endday} napjan vegzodik")
+    def WriteDate(self):
+        self.Holidays()
+        print(f"A hatarido vege {self.endday} napjan vegzodik")
 
 
-WriteDate()
+idopont = input("Irja be a hatarozat idopontjat kotojelekkel elvalasztva: ")
+hatarozat1 = AppealCalculator(idopont)
+hatarozat1.WriteDate()
